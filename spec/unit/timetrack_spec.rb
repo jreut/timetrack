b1a03f9c17a57cb1639db8131a7ae3f8efb6a13e
Jordan Ryan Reuter <jordan.reuter@scimedsolutions.com>
2017-01-20 16:32:49 -0500

    Setup rspec

--- spec/unit/timetrack_spec.rb
+++ spec/unit/timetrack_spec.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-describe Timetrack do
+RSpec.describe Timetrack do
   it 'has a version number' do
     expect(Timetrack::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
