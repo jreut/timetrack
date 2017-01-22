5fc68aa0ea4c0322e3ee60f661bf4c637f909e21
Jordan Ryan Reuter <jordan.reuter@scimedsolutions.com>
2017-01-21 19:26:14 -0500

    Make integration spec use gem entrypoint

--- spec/integration/parser/parse_spec.rb
+++ spec/integration/parser/parse_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require 'spec_helper'
-require 'timetrack/parser'
+require 'timetrack'
 
 RSpec.describe Timetrack::Parser, '#parse' do
   let(:object) { described_class.new }
